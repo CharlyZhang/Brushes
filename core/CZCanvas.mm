ca6586e0fcea750fa7abaefb96c05f8b3c5b5546
CharlyZhang <chicboi@hotmail.com>
2015-04-20 18:38:18 +0800

    ＃设计Canvas。修改了CZShader，增强了CZFbo，使其能支持外部改变renderstorage；实现了利用canvas展示纹理图。

---
+++ core/CZCanvas.mm
@@ -0,0 +1,42 @@
+
+///  \file CZCanvas.cpp
+///  \brief This is the file implements the class CZCanvas.
+///
+///	
+///
+///  \version	1.0.0
+///	 \author	Charly Zhang<chicboi@hotmail.com>
+///  \date		2014-10-15
+///  \note
+
+#include "CZCanvas.h"
+#include "painting/CZPainting.h"
+
+/// CZView, 平台相关的视图
+struct CZCanvas::CZView
+{
+	CZView(){};
+	~CZView(){};
+};
+
+CZCanvas::CZCanvas()
+{
+	view = new CZView;
+}
+
+CZCanvas::~CZCanvas()
+{
+	delete view;
+}
+
+/// 绘制视图
+void CZCanvas::drawView()
+{
+	drawViewInRect(visibleRect);
+}
+
+/// 在一定区域绘制视图
+void CZCanvas::drawViewInRect(CZRect &rect)
+{
+
+}
