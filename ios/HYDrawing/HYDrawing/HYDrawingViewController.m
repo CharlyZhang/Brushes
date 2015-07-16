e71be81766deab03feab0244ba8d5f9669e43c18
lileichuan <506396403@qq.com>
2015-07-16 19:11:38 +0800

    drawing图片提交。

---
+++ ios/HYDrawing/HYDrawing/HYDrawingViewController.m
@@ -0,0 +1,51 @@
+//
+//  HYDrawingViewController.m
+//  HYDrawing
+//
+//  Created by 李 雷川 on 15/7/16.
+//  Copyright (c) 2015年 Founder. All rights reserved.
+//
+
+#import "HYDrawingViewController.h"
+
+@interface HYDrawingViewController ()
+
+@end
+
+@implementation HYDrawingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    UIBarButtonItem *menuItem = [[UIBarButtonItem alloc]initWithImage:[UIImage imageNamed:@"menu"] style:UIBarButtonItemStylePlain target:self action:@selector(tapMenu:)];
+    
+    self.navigationItem.leftBarButtonItems = @[menuItem];
+    
+    UIBarButtonItem *videoItem = [[UIBarButtonItem alloc]initWithImage:[UIImage imageNamed:@"video"] style:UIBarButtonItemStylePlain target:self action:@selector(tapMenu:)];
+    UIBarButtonItem *settingItem = [[UIBarButtonItem alloc]initWithImage:[UIImage imageNamed:@"setting"] style:UIBarButtonItemStylePlain target:self action:@selector(tapMenu:)];
+    UIBarButtonItem *imageItem = [[UIBarButtonItem alloc]initWithImage:[UIImage imageNamed:@"picture"] style:UIBarButtonItemStylePlain target:self action:@selector(tapMenu:)];
+    UIBarButtonItem *shareItem = [[UIBarButtonItem alloc]initWithImage:[UIImage imageNamed:@"share"] style:UIBarButtonItemStylePlain target:self action:@selector(tapMenu:)];
+    self.navigationItem.rightBarButtonItems = @[shareItem,imageItem,settingItem,videoItem];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (void)tapMenu:(id)sender{
+    
+}
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
